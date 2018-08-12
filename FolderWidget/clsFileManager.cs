1394fdddb479acbd6c09898b7fb5829e2dca8928
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2018-08-12 12:45:26 +0300

    Initial commit

---
+++ FolderWidget/clsFileManager.cs
@@ -0,0 +1,200 @@
+﻿using Shell32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderWidget
+{
+    static class clsFileManager
+    {
+
+        static public bool CreateIfDoesntExists()
+        {
+            CreateFolder();
+            return CreateFile();
+        }
+        static public DataTable GetAllLines()
+        {
+            try
+            {
+                if (File.Exists(ConfigurationManager.AppSettings["mainPath"]))
+                {
+                    DataSet ds = new DataSet();
+                    ds.ReadXml(ConfigurationManager.AppSettings["mainPath"]);
+                    return ds.Tables[0];
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                WriteError("GetAllLines: " + ex.Message, ex.StackTrace);
+                return null;
+            }
+        }
+        static public DataTable GetInitData()
+        {
+            CreateIfDoesntExists();
+            return GetAllLines();
+        }
+        static private bool CreateFile()
+        {
+            try
+            {
+                if (!File.Exists(ConfigurationManager.AppSettings["mainPath"]))
+                {
+                    DataTable dt = new DataTable("icons");
+                    dt.Columns.Add("Path");
+                    dt.Columns.Add("Icon");
+                    dt.Columns.Add("User");
+                    dt.Columns.Add("Pass"); //should encrypt
+                    dt.WriteXmlSchema(ConfigurationManager.AppSettings["mainPath"]);
+                    WriteError("Added xml File", ConfigurationManager.AppSettings["mainPath"]);
+                    File.SetAttributes(ConfigurationManager.AppSettings["mainPath"], File.GetAttributes(ConfigurationManager.AppSettings["mainPath"]) | FileAttributes.Hidden);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                WriteError("Create FIle: " + ex.Message, ex.StackTrace);
+                return false;
+            }
+        }
+        static private bool CreateFolder()
+        {
+            try
+            {
+                string folderPath = Path.GetDirectoryName(ConfigurationManager.AppSettings["mainPath"]);
+                if (!Directory.Exists(folderPath))
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(folderPath);
+                    WriteError("Added Folder", folderPath);
+                    di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                WriteError(ex.Message, ex.StackTrace);
+                return false;
+            }
+        }
+        public static string GetShortcutTargetFile(string shortcutFilename)
+        {
+            try
+            {
+                string pathOnly = Path.GetDirectoryName(shortcutFilename);
+                string fileNameOnly = Path.GetFileName(shortcutFilename);
+
+                Shell shell = new Shell();
+                Folder folder = shell.NameSpace(pathOnly);
+                FolderItem folderItem = folder.ParseName(fileNameOnly);
+                if (folderItem != null)
+                {
+                    ShellLinkObject link = (ShellLinkObject)folderItem.GetLink;
+                    return link.Path;
+                }
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                WriteError("GetShortcutTargetFile: " + ex.Message, ex.StackTrace);
+                return string.Empty;
+            }
+        }
+        static public void ReplaceXMLFile(DataTable dt)
+        {
+            try
+            {
+                File.Delete(ConfigurationManager.AppSettings["mainPath"]);
+                if (dt.Rows.Count > 0)
+                {
+                    dt.WriteXml(ConfigurationManager.AppSettings["mainPath"]);
+                }
+                else
+                {
+                    dt.WriteXmlSchema(ConfigurationManager.AppSettings["mainPath"]);
+                }
+                WriteError("Xml file updated",ConfigurationManager.AppSettings["mainPath"]);
+                File.SetAttributes(ConfigurationManager.AppSettings["mainPath"], File.GetAttributes(ConfigurationManager.AppSettings["mainPath"]) | FileAttributes.Hidden);
+            }
+            catch (Exception ex)
+            {
+                WriteError("ReplaceXMLFile: " + ex.Message, ex.StackTrace);
+            }
+        }
+
+
+        static public int SaveIcon(ref DataTable dt,Image icon)
+        {
+            try
+            {
+                int max = 0;
+                try
+                {
+                    max = Convert.ToInt32(dt.AsEnumerable().Max(row => row["icon"]));
+                }
+                catch (Exception)
+                {
+                    max = 0;
+                }
+                if (!Directory.Exists(ConfigurationManager.AppSettings["iconPath"]))
+                {
+                    Directory.CreateDirectory(ConfigurationManager.AppSettings["iconPath"]);
+                }
+                icon.Save(ConfigurationManager.AppSettings["iconPath"] + (max + 1) + ".png");
+                WriteError("icon added", ConfigurationManager.AppSettings["iconPath"] + (max + 1) + ".png");
+                return max + 1;
+            }
+            catch (Exception ex)
+            {
+                WriteError("SaveIcon: " + ex.Message, ex.StackTrace);
+                return -1;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void WriteError(string message, string stackTrace)
+        {
+            try
+            {
+                string msg = Environment.NewLine + DateTime.Now.ToString() + Environment.NewLine + "Message: " + message + Environment.NewLine + "StackTrace: " + stackTrace + Environment.NewLine + Environment.NewLine + "───────────────────────────────────────────────────────────────────────────────────────────" + Environment.NewLine;
+                File.AppendAllText(ConfigurationManager.AppSettings["errorPath"], msg);
+                if ((File.GetAttributes(ConfigurationManager.AppSettings["mainPath"]) & FileAttributes.Hidden) != FileAttributes.Hidden)
+                {
+                    File.SetAttributes(ConfigurationManager.AppSettings["mainPath"], File.GetAttributes(ConfigurationManager.AppSettings["mainPath"]) | FileAttributes.Hidden);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("התרחשה שגיאה לא צפויה", "התרחשה שגיאה לא צפויה", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
---
+++ FolderWidget/clsManageComunication.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderWidget
+{
+
+
+    public static class clsManageComunication
+    {
+
+        public static BackgroundWorker m_ServerWorker;
+        public static BackgroundWorker GetServerWorker
+        {
+            get
+            {
+                if (m_ServerWorker == null)
+                {
+                    m_ServerWorker = new BackgroundWorker();
+                    m_ServerWorker.DoWork += M_ServerWorker_DoWork;
+                }
+                return m_ServerWorker;
+            }
+        }
+
+
+
+        public static void M_ServerWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.InOut, 4);
+            StreamReader sr = new StreamReader(pipeServer);
+            StreamWriter sw = new StreamWriter(pipeServer);
+            do
+            {
+                try
+                {
+                    pipeServer.WaitForConnection();
+                    pipeServer.WaitForPipeDrain();
+                    SendMessage(sr.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    pipeServer.WaitForPipeDrain();
+                    if (pipeServer.IsConnected)
+                    {
+                        pipeServer.Disconnect();
+                    }
+                }
+            } while (true);
+        }
+
+        public static void TransmitDataToServer(string data)
+        {
+            var pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut, PipeOptions.Asynchronous);
+            if (pipeClient.IsConnected != true) { pipeClient.Connect(); }
+            StreamReader sr = new StreamReader(pipeClient);
+            StreamWriter sw = new StreamWriter(pipeClient);
+            try
+            {
+                sw.WriteLine(Guid.NewGuid());
+                sw.Flush();
+                pipeClient.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+        public delegate void dlSendMessage(string Message);
+        public static event dlSendMessage OnSendMessage;
+        public static void SendMessage(string Message)
+        {
+            if (OnSendMessage != null)
+            {
+                OnSendMessage(Message);
+            }
+        }
+    }
+}
